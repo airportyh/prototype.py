e9268930848f08b5911d42f47400ddd09ba96bc9
Matěj Cepl <mcepl@cepl.eu>
2015-11-10 12:32:15 +0100

    PEP8isation.
    Cleaned up the code to be PEP8 compliant as much as possible without
    violating the code too much.

--- classy.py
+++ classy.py
@@ -1,42 +1,47 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright © 2011 Toby Ho
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy 
-# of this software and associated documentation files (the “Software”), to deal 
-# in the Software without restriction, including without limitation the rights 
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-# copies of the Software, and to permit persons to whom the Software is furnished 
-# to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all 
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# “Software”), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-from prototype import *
+from prototype import *  # noqa
+
 
 def Class(func):
-  attrs = func()
-  cls = constructor(attrs['init'])
-  del attrs['init']
-  cls.__name__ = func.__name__
-  for name in attrs.keys():
-    setattr(cls.prototype, name, attrs[name])
-  return cls
-  
+    attrs = func()
+    cls = constructor(attrs['init'])
+    del attrs['init']
+    cls.__name__ = func.__name__
+    for name in attrs.keys():
+        setattr(cls.prototype, name, attrs[name])
+    return cls
+
+
 @Class
 def Show():
-  def init(this, title):
-    this.title = title
-    this.personnel = []
-  def addPersonnel(this, person):
-    this.personnel.append(person)
-  return locals()
-  
+    def init(this, title):
+        this.title = title
+        this.personnel = []
+
+    def addPersonnel(this, person):
+        this.personnel.append(person)
+
+    return locals()
